85297ee8a56a430f64bb80647b8280e7ad79842a
MirAfzalul <mirafzalulbashar@gmail.comgit config --global user.email mirafzalulbashar@gmail.com>
2021-08-05 22:45:12 +0600

    Tutorial-6-Basic Class Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Greets from './components/Greet'
-
+import Welcome from './components/Welcome'
 function App() {
   return (
     <div className="App">
       <Greets />
+      <Welcome/>
     </div>
   );
 }
