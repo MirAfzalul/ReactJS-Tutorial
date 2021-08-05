08847f1a626e9ed29377ca8d2e1526718cdf1fc3
MirAfzalul <mirafzalulbashar@gmail.comgit config --global user.email mirafzalulbashar@gmail.com>
2021-08-06 01:08:05 +0600

    Tutorial-8-Without JSX

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Greets from './components/Greet'
 import Welcome from './components/Welcome'
-// import Hello from '/components/Hello'
+import Hello from './components/Hello'
 function App() {
   return (
     <div className="App">
-      <Greets />
-      <Welcome/>
-      {/* <Hello/> */}
+      {/* <Greets /> */}
+      {/* <Welcome/> */}
+      <Hello/>
     </div>
   );
 }
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 const Hello = () => {
-    return(
-        <div className='dummyClass'>
-            <h1>Hello Asif</h1>
-        </div>
+    // return(
+    //     // <div className='dummyClass'>
+    //     //     <h1>Hello Asif</h1>
+    //     // </div>
+
+    // )
+    return React.createElement(
+        'div',
+        {id: 'hello', className: 'DummyClass'},
+        React.createElement('h1',null,'Hello Bashar')
     )
 }
-// export default Hello 
+export default Hello 
