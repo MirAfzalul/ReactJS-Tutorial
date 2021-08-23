7cbcad66fdf3da16c0a032359d30f278c6ce9744
MirAfzalul <mirafzalulbashar@gmail.comgit config --global user.email mirafzalulbashar@gmail.com>
2021-08-23 10:44:30 +0600

    Tutorial - 20 - Styling and CSS Basics Part-2

--- src/App.js
+++ src/App.js
@@ -12,10 +12,17 @@ import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
 import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
+
 function App() {
   return (
     <div className="App">
       <Stylesheet primary={true}/>
+      <Inline/>
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
       {/* <Greets name= "asif" >
       <p>This is a paragraph</p>
       </Greets>
